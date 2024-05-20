c7246f547a5f4a7e9b2532ad70c03dcca864d2e4
ha2way <gusdkfk@naver.com>
2024-05-20 17:33:02 +0900

    ExhibitScape 폴더구조 작성 프로젝트

---
+++ ExhibitScape/src/main/java/com/ExhibitScape/app/ExhibitScapeApplication.java
@@ -0,0 +1,13 @@
+package com.ExhibitScape.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExhibitScapeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExhibitScapeApplication.class, args);
+	}
+
+}
---
+++ ExhibitScape/src/test/java/com/ExhibitScape/app/ExhibitScapeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ExhibitScape.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExhibitScapeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
